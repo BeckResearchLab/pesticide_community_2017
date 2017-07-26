da5e94253a6fbc68ca9e8448f602a7aab659b9aa
David Beck <dacb@uw.edu>
2017-07-26 09:53:23 -0700

    prototype sample name processing

--- 1.pipeline.R
+++ 1.pipeline.R
@@ -6,10 +6,10 @@ list.files(path)
 # expect a list of files in the data directory
 
 # Sort ensures forward/reverse reads are in same order
-fnFs <- sort(list.files(path, pattern="_R1_001.fastq"))
-fnRs <- sort(list.files(path, pattern="_R2_001.fastq"))
-# Extract sample names, assuming filenames have format: SAMPLENAME_XXX.fastq
-sample.names <- sapply(strsplit(fnFs, "_"), `[`, 1)
+fnFs <- sort(list.files(path, pattern="_R1_001.no_barcodes"))
+fnRs <- sort(list.files(path, pattern="_R2_001"))
+sample.names <- sapply(strsplit(fnFs, "_"), `[`, 7)
+print(sapply(strsplit(sample.names, ".", fixed = TRUE), `[`, 1))
 # Specify the full path to the fnFs and fnRs
 fnFs <- file.path(path, fnFs)
 fnRs <- file.path(path, fnRs)
